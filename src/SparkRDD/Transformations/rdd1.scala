6cbf5519215a4627550bb368b8b06651f209d8ea
houjingru <1627383642@qq.com>
2016-07-09 14:46:45 +0800

    2016-7-9 添加RDD丙型集合操作实例

--- src/SparkRDD/Transformations/rdd1.scala
+++ src/SparkRDD/Transformations/rdd1.scala
@@ -10,7 +10,33 @@ object rdd1 {
     val conf = new SparkConf().setAppName("hjr").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val data = sc.textFile("D:/sparkLab/Resources/data/1.txt")
+    /**
+      *并行集合操作实例
+      *
+      *
+      * 并行集合（Parallelized collections）的创建是通过在一个已有的集合（Scala Seq）上调用SparkContext的parallelize方法实现的
+      *
+      * 集合中的元素被复制到一个可并行操作的分布式数据集中
+      *
+      * 一旦创建完成，这个分布式数据集（distData）就可以被并行操作
+      *
+      * 我们可以调用distData.reduce((a, b) => a + b),将数组中的元素相加
+      * 或者这种形式也行：distData.reduce((_ + _))
+      *
+      * 并行集合一个很重要的的参数是切片数（slices），表示一个数据集切分的份数
+      * Spark会在集群上为每一个切片运行一个任务，我们可以在集群上为每个CPU设置2-4个切片（slices）
+      * 正常情况下，Spark会试着基于你的集群状况自动的设置切片的数目，
+      * 然而，我们也可以通过parallelize的第二个参数手动设置（eg:sc.parallelize(data, 10)）
+      */
+    val data1 = Array(1, 2, 3, 4, 5)
+    val distData = sc.parallelize(data1)
+    println(distData.reduce((a, b)=> a + b))
+    println(distData.reduce((_+_)))
+
+    /**
+      * flatmap算子操作实例
+      */
+    val data = sc.textFile("Resources/data/1.txt")
     data.foreach(println)
 
     data.flatMap(line =>{
@@ -20,5 +46,8 @@ object rdd1 {
     data.flatMap(line =>(line.split("\t"))).map(word =>(word, 1)).reduceByKey(_ + _).foreach(println)
 
     data.flatMap(line =>(line.split("\t"))).map(word =>(word,1)).reduceByKey((a, b) => a + b).foreach(println)
+
+
+
   }
 }
