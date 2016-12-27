2a65460436ae27160ce4bec13e544704a1fca92e
hjr <1627383642@qq.com>
2016-12-27 22:50:32 +0800

    2016-12-27 添加MLLib_Statistics相关操作---注释说明1

--- src/SparkMLLib/MLLib_Statistics.scala
+++ src/SparkMLLib/MLLib_Statistics.scala
@@ -19,20 +19,60 @@ object MLLib_Statistics {
     /**
       * 读取数据，转换成RDD[Vector]类型
       *
+      * 注意：
+      *
+      * 在这个编辑器里面的“/t”不是标准的分隔符，识别不了
+      * 所以这里我换成了“ ”空格分隔符
+      *
       */
-    val data_path="Resources/data/Statistics/sample_stat.txt"
+    val data_path="Resources/data/Statistics/sample_stat.txt"//本地数据文件路径
+
+    /**
+      * 打印原始数据
+      */
+    sc.textFile(data_path).foreach(println)
+    /**
+      * （1）使用textFile("数据文件路径")加载数据
+      * （2）使用map(“分隔符”)操作分割/映射每一行的数据
+      * （3）使用map("自定义函数")操作
+      * （4）map("自定义函数")：（输入为每一行中的每一列数据）
+      *       {
+      *         f => f.map(f => f.toDouble)
+      *       }
+      *       每一行的数据通过分隔符映射出来以后，
+      *       对每一个数据，再次做map映射，
+      *       这次的map映射中的自定义函数为：（输入为每一列数据）
+      *       {
+      *         f => f.toDouble
+      *       }
+      *       对每一个映射出来的数据进行格式转换，转化为Double型数据
+      */
 
     val data = sc.textFile(data_path).map(_.split(" ")).map(f => f.map(f => f.toDouble))
 
+    /**
+      * 继续map操作
+      * {
+      *   f => Vectors.dense(f)
+      * }
+      * map("自定义函数")：
+      *
+      * 输入为map的每一行数据
+      *
+      * 输出为一个密度向量（由一个Double型的数组转化而来）
+      *
+      * Creates a dense vector from a double array
+      *
+      */
     val data1 = data.map(f => Vectors.dense(f))
 
     val stat1 = Statistics.colStats(data1)
-    stat1.max
-    stat1.min
-    stat1.mean
-    stat1.variance
-    stat1.normL1
-    stat1.normL2
+    println("每一列的最大值： "+stat1.max)
+    println("每一列的最小值： "+stat1.min)
+    println("每一列的平均值： "+stat1.mean)
+    println("每一列的方差值： "+stat1.variance)
+    println("L1范数： "+stat1.normL1)
+    println("L2范数： "+stat1.normL2)
   }
 
 }
