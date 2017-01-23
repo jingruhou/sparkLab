cfbc2051b7b3eaeef407b6562d317c0745ea3382
hjr <1627383642@qq.com>
2017-01-23 23:48:07 +0800

    2017-1-23 测试运行并注释Spark官网例子程序HdfsWordCount

--- src/SparkStreaming/HdfsWordCount.scala
+++ src/SparkStreaming/HdfsWordCount.scala
@@ -1,30 +1,41 @@
 package SparkStreaming
 
 import org.apache.spark.SparkConf
+import org.apache.spark.examples.streaming.StreamingExamples
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 /**
   * Created by Administrator on 2016/6/14.
   */
 object HdfsWordCount {
   def main(args: Array[String]) {
-    if (args.length < 1) {
+    /*if (args.length < 1) {
       System.err.println("Usage: HdfsWordCount <directory>")
       System.exit(1)
-    }
-
-    //StreamingExamples.setStreamingLogLevels()
-    val sparkConf = new SparkConf().setAppName("HdfsWordCount").setMaster("local")
+    }*/
+    //设置输出日志级别
+    StreamingExamples.setStreamingLogLevels()
+    //创建SparkConf对象
+    val sparkConf = new SparkConf().setAppName("HdfsWordCount").setMaster("local[2]")
     // Create the context
-    val ssc = new StreamingContext(sparkConf, Seconds(5))
+    //创建StreamingContext对象，与集群进行交互
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
 
     // Create the FileInputDStream on the directory and use the
     // stream to count words in new files created
+    //如果目录中有新创建的文件，则读取
     //val lines = ssc.textFileStream(args(0))
-    val lines = ssc.textFileStream("D:/streamingData/WordCount")
+    val lines = ssc.textFileStream("Resources/Streaming/HdfsWordCount/")
+
+    //将每一行数据分割为单词（分隔符为空格）
     val words = lines.flatMap(_.split(" "))
+    //统计单词出现的次数
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    //打印结果
     wordCounts.print()
+    //启动Spark Streaming
     ssc.start()
+    //一直运行，除非人为干预再停止
     ssc.awaitTermination()
   }
+
 }
--- src/StreamingMLLib/DataGenerator_StreamingKMeans.scala
+++ src/StreamingMLLib/DataGenerator_StreamingKMeans.scala
@@ -17,8 +17,23 @@ object DataGenerator_StreamingKMeans {
 
     /**
       *  1 循环生成数据
+      *
+      *  具体步骤如下：
+      *
+      *  1）随机生成一条KMeansStreamingRDD的数据（数据的簇数量、维度等参数自行设定，或根据已有模型设定）
+      *
+      *  2）初始化StreamingKMeans模型
+      *
+      *  3）训练模型
+      *
+      *  4）预测数据
+      *
+      *  5）
+      *
+      *  6）问题：这个就不是StreamingKMeans在线计算了，而是死循环了
       */
     while(true){
+
       val KMeansStreamingRDD = KMeansDataGenerator.generateKMeansRDD(sc,1,5,3,1.0,1).map(f => Vectors.dense(f))
     }
 
