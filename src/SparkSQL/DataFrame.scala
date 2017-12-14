f25e15258ac8b9ca385d03cf5a783878f66f31c2
hjr <1627383642@qq.com>
2017-12-14 16:19:05 +0800

    2017-12-14 houjingru@xunfang.com spark-submit --master yarn-client --class SparkSQL.DataFrame --executor-memory  256m sparkLab.jar

--- src/SparkSQL/DataFrame.scala
+++ src/SparkSQL/DataFrame.scala
@@ -9,13 +9,13 @@ import org.apache.spark.{SparkContext, SparkConf}
 object DataFrame {
   def main(args:Array[String]): Unit ={
 
-    // 0 初始化环境配置
-    val conf = new SparkConf().setAppName("zkjz_df_hjr")
+    // 0 初始化环境配置 spark://hjr:7077"
+    val conf = new SparkConf().setAppName("houjingru_CDH5_Cluster_StockApps")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
     // 1 加载数据文件 --- 中国 A 股上市公司基本信息
-    val df = sqlContext.read.json("/user/houjr/data/stock_5.json")
+    val df = sqlContext.read.json("/user/hjr/temp/stock_5.json")
 
     // 2 - 1打印 DataFrame 的 Schema
     df.printSchema()
@@ -70,7 +70,6 @@ object DataFrame {
     df.registerTempTable("stock")
 
     sqlContext.sql("select * from stock").show(5)
-
 
   }
 }
